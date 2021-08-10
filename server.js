054173e7d4e2d249bf40a63d052dfb71c57617ef
timdavidson2 <timdavidson2@gmail.com>
2021-08-10 12:47:06 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+// add variable for modules
+const express = require("express");
+const path = require("path");
+
+// set variable for express and PORT
+const app = express();
+const PORT = process.env.PORT || 3000;
