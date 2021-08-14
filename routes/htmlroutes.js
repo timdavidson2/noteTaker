92964812788ac2de5663ce17e0d361cf2475d759
Tim Davidson <timdavidson2@gmail.com>
2021-08-13 21:06:41 -0400

    Update htmlroutes.js

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const express = require('express');
+const app = express.Router();
 
-module.exports = (app) => {
+
+// module.exports = (app) => {
 	//Main file for home page.
 	app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 	//Send to notes page.
 	app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
 	//If no matching route, then go back to main page.
 	app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
-};
+// };
+
+module.exports = app;
